7c7f1b890f76d0874f604fb885d2d725cf48d914
Jostein Tysse <josteiht@stud.ntnu.no>
2021-09-03 19:53:00 +0200

    Calculate best attribute

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -1,67 +1,88 @@
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
 # IMPORTANT: DO NOT USE ANY OTHER 3RD PARTY PACKAGES
 # (math, random, collections, functools, etc. are perfectly fine)
 
 
 class DecisionTree:
-    
+
     def __init__(self):
-        # NOTE: Feel free add any hyperparameters 
+        # NOTE: Feel free add any hyperparameters
         # (with defaults) as you see fit
         self.root = {}
-    
+
     def fit(self, X, y):
         """
         Generates a decision tree for classification
-        
+
         Args:
             X (pd.DataFrame): a matrix with discrete value where
                 each row is a sample and the columns correspond
                 to the features.
             y (pd.Series): a vector of discrete ground-truth labels
         """
-        # TODO: Implement 
+        # TODO: Implement
+
+        attributes = X.columns
+        GOAL_ATTRIBUTE = y.name
+
+        # Number of values for the goal attribute
+        S = np.sum(y.value_counts().values)
+
+        if (y.value_counts().count() == 1):  # Entropy is 0, we only have one type of goal attributes
+            self.root["label"] = y.value_counts().values()[0]
+            return
+
+        # The entropy of all the examples
+        dataset_entropy = entropy(y.value_counts().values)
+
+        # Make a single dataframe, as it is easy to use Pandas functions on this to get the desired values
+        data = X.join(y)
 
         A = ""
-        val = 0
-        split = None
+        val = 0.
 
-        p = y.value_counts()[1]
-        n = y.value_counts()[0]
+        for a in attributes:
 
+            grouped = data.groupby(a)[
+                GOAL_ATTRIBUTE].value_counts().unstack(fill_value=0).stack()  # group data and fill in 0 if needed
 
+            gain = dataset_entropy
+            for sv in X[a].unique():
+                gain -= float(np.sum(grouped[sv])) / S * \
+                    entropy(grouped[sv].values)
 
-
-
+            if gain >= val:
+                A = a
+                val = gain
 
     def predict(self, X):
         """
         Generates predictions
-        
+
         Note: should be called after .fit()
-        
+
         Args:
             X (pd.DataFrame): an mxn discrete matrix where
                 each row is a sample and the columns correspond
                 to the features.
-            
+
         Returns:
             A length m vector with predictions
         """
-        # TODO: Implement 
+        # TODO: Implement
         #raise NotImplementedError()
-    
+
     def get_rules(self):
         """
         Returns the decision tree as a list of rules
-        
+
         Each rule is given as an implication "x => y" where
         the antecedent is given by a conjuction of attribute
         values and the consequent is the predicted label
-        
+
             attr1=val1 ^ attr2=val2 ^ ... => label
-        
+
         Example output:
         >>> model.get_rules()
         [
@@ -74,16 +95,16 @@ class DecisionTree:
         raise NotImplementedError()
 
 
-# --- Some utility functions 
-    
+# --- Some utility functions
+
 def accuracy(y_true, y_pred):
     """
     Computes discrete classification accuracy
-    
+
     Args:
         y_true (array<m>): a length m vector of ground truth labels
         y_pred (array<m>): a length m vector of predicted labels
-        
+
     Returns:
         The average number of correct predictions
     """
@@ -94,24 +115,21 @@ def accuracy(y_true, y_pred):
 def entropy(counts):
     """
     Computes the entropy of a partitioning
-    
+
     Args:
-        counts (array<k>): a lenth k int array >= 0. For instance,
+        counts (array<k>): a length k int array >= 0. For instance,
             an array [3, 4, 1] implies that you have a total of 8
             datapoints where 3 are in the first group, 4 in the second,
             and 1 one in the last. This will result in entropy > 0.
             In contrast, a perfect partitioning like [8, 0, 0] will
             result in a (minimal) entropy of 0.0
-            
+
     Returns:
         A positive float scalar corresponding to the (log2) entropy
         of the partitioning.
-    
+
     """
     assert (counts >= 0).all()
     probs = counts / counts.sum()
     probs = probs[probs > 0]  # Avoid log(0)
     return - np.sum(probs * np.log2(probs))
-
-
-
